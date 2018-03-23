86ef684d647b69ae3a74af194b3812ede1b91009
Thirty33 <joelsuarez.1101@gmail.com>
2018-03-23 09:17:29 -0500

    collections y sus tipos

--- institucion/Program.cs
+++ institucion/Program.cs
@@ -4,12 +4,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace institucion
 {
     class Program
     {
-        static void Main(string[] args)
+        public void rutina1()
         {
             Console.WriteLine("Gestion de Institucion");
             var alumno1 = new Alumno("joel", "suarez")
@@ -48,7 +49,19 @@ namespace institucion
             Console.WriteLine(alumno1.ConstruirResumen());
             Console.WriteLine(profesor.ConstruirResumen());
 
+            Console.WriteLine("retornando valores por referencia");
+            var anum = 15;
+            Console.WriteLine(alumno1.RetornarVal(ref anum));
+            Console.WriteLine(anum);
 
+            Console.WriteLine("retornando valores sin referencia");
+            Console.WriteLine(alumno1.RetornarValWReference(anum));
+            Console.WriteLine(anum);
+
+        }
+
+        public void rutina2()
+        {
             Console.WriteLine("Estructuras");
             CursoStruct c = new CursoStruct(70);
             c.Curso = "101-B";
@@ -78,14 +91,7 @@ namespace institucion
             Console.WriteLine($"Curso freak = {cursoFreak_class.Curso}");
 
 
-            Console.WriteLine("retornando valores por referencia");
-            var anum = 15;
-            Console.WriteLine(alumno1.RetornarVal(ref anum));
-            Console.WriteLine(anum);
-
-            Console.WriteLine("retornando valores sin referencia");
-            Console.WriteLine(alumno1.RetornarValWReference(anum));
-            Console.WriteLine(anum);
+            
 
             Console.WriteLine("Enumeraciones");
             var alumnoEst = new Alumno("gustavo", "alarcon")
@@ -102,15 +108,15 @@ namespace institucion
             Console.WriteLine("el estado del alumno es" +
                 alumnoEst.Estado);
 
-            Console.WriteLine($"TIPO: {typeof (EstadosAlumno)}");
-            Console.WriteLine($"TIPO: {typeof (Alumno)}");
+            Console.WriteLine($"TIPO: {typeof(EstadosAlumno)}");
+            Console.WriteLine($"TIPO: {typeof(Alumno)}");
 
             Console.WriteLine($"TIPO: {alumnoEst.GetType()}");
             Persona prueba = alumnoEst;
             Console.WriteLine($"TIPO prueba : {prueba.GetType()}");
 
-            Console.WriteLine($"TIPO: {nameof (Alumno)}");
-            Console.WriteLine($"TIPO: {sizeof (int)}");
+            Console.WriteLine($"TIPO: {nameof(Alumno)}");
+            Console.WriteLine($"TIPO: {sizeof(int)}");
 
 
             Console.WriteLine("Casteo de datos");
@@ -126,19 +132,22 @@ namespace institucion
 
             float f = 2.35f;
             Console.WriteLine(i);
-            s = (short) i;
+            s = (short)i;
             Console.WriteLine(s);
 
             Console.WriteLine(f);
             i = (int)f;
             Console.WriteLine(i);
 
+        }
 
+        public void RutinaArreglos()
+        {
             Console.WriteLine("Arreglos");
             Persona[] arregloPersonas = new Persona[5];
             Console.WriteLine(arregloPersonas.Length);
 
-            arregloPersonas[0] = new Alumno("pedro", "fernandez");
+            arregloPersonas[0] = new Alumno("pedro", "fernandez") { Id = 1, Edad = 36, NickName = "JOEKS", Telefono = "22333222", Email = "joelsuarez.1101@gmail.com" };
             arregloPersonas[1] = new Profesor() { Nombre = "Fritz", Apellido = "Perls" };
             arregloPersonas[2] = new Alumno("", "fernandez");
             arregloPersonas[3] = new Alumno("Juan", "fernandez");
@@ -146,27 +155,62 @@ namespace institucion
 
             for (int cont = 0; cont < arregloPersonas.Length; cont++)
             {
-                if(arregloPersonas[cont] is Alumno)
+                if (arregloPersonas[cont] is Alumno)
 
                 {
                     var al = (Alumno)arregloPersonas[cont];
-                    Console.WriteLine(al.NickName != null? al.NickName : al.NombreCompleto);
+                    Console.WriteLine(al.NickName != null ? al.NickName : al.NombreCompleto);
                 }
                 else
                 {
                     Console.WriteLine(arregloPersonas[cont].NombreCompleto);
                 }
-                
+
             }
-            Console.ReadLine();
 
-           
+        }
 
+        private static void RutinaCollections()
+        {
+            System.Collections.ArrayList ListaPersonas = new System.Collections.ArrayList();
+            var Persona2 = new Alumno("Juan", "fernandez");
+            var Persona3 = new Alumno("Joel", "Suarez");
+            ListaPersonas.Add(Persona2);
+            ListaPersonas.Add(new Alumno("Carlos", "Martinez"));
+            ListaPersonas.Add(Persona3);
+            // One Object Type
+            List<Persona> ListaPersonas2 = new List<Persona>();
+            List<object> ListaGenerica = new List<object>();
+
+
+            Console.WriteLine(ListaPersonas);
+            for (int i = 0; i < ListaPersonas.Count; i++)
+            {
+                var per = ListaPersonas[i] as Persona;
+
+                if (per != null)
+                    Console.WriteLine(per.NombreCompleto);
+            }
+
+            foreach (var item in ListaPersonas)
+            {
+                if (item is Alumno)
+                {
+                    var al = (Alumno)item;
+                    Console.WriteLine(al.NickName != null ? al.NickName : al.NombreCompleto);
+
+                }
+
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
 
             
 
-
-
+            Console.ReadLine();
 
         }
     }
